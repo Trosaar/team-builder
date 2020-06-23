bbba37590ab395cb410f84a1346adf5264a369e1
Aaron T <troskaaaron@gmail.com>
2020-06-22 19:19:43 -0500

    added some files for later and started working on content.

--- team-builder-app/src/App.js
+++ team-builder-app/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Dash from './components/dash'
+import { Container } from 'reactstrap';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Header</p>
+			</header>
+			
+			<Container className="main">
+				<Dash />
+			</Container>
+
+			<footer className="App-footer">
+				<p>footer</p>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
---
+++ team-builder-app/src/components/dash.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Container, Row, Col } from 'reactstrap';
+
+const Dashboard = () => {
+	return (
+		<Container className="dash">
+			<Col>
+				<Row>
+					<p>stuff</p>
+				</Row>
+			</Col>
+		</Container>
+	)
+}
+
+export default Dashboard
