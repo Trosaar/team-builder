be3ba04334492320ec07442f3b99d10e8e2e3ea8
Aaron T <troskaaaron@gmail.com>
2020-06-24 18:27:39 -0500

    Formik Form throwing an error about handleChance. Decided to use html form instead of Formik form. Text field working with label

--- team-builder-app/src/components/dash.js
+++ team-builder-app/src/components/dash.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Button } from 'reactstrap';
+import { useFormik } from 'formik';
 
 const Dashboard = () => {
+
+	const Formik = useFormik({
+		initialValues: {
+			firstName: '',
+			lastName: '',
+			email: '',
+		},
+		onsubmit: value => {
+			alert(JSON.stringify(value, null, 2))
+		},
+	})
+
 	return (
 		<Container>
 			<Col>
 				<Row>
-					<p>stuff</p>
+					<form onSubmit={Formik.handleSubmit}>
+						<label htmlFor="name">Name</label>
+						<input id='name' name='name' type='text' onChange={Formik.handleChange} value={Formik.values.name} />
+						<Button type='reset' onClick={Formik.handleReset}>Reset</Button>
+					</form>
 				</Row>
 			</Col>
 		</Container>
