758aed3b34ee49663baae9826db3e9718585ec5c
Aaron T <troskaaaron@gmail.com>
2020-06-25 19:03:50 -0500

    less working

--- team-builder-app/src/App.js
+++ team-builder-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './css/index.css';
 import { Route } from 'react-router-dom';
 import Dash from './components/dash'
 
--- team-builder-app/src/index.js
+++ team-builder-app/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
