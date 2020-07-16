5282009abf88dc573cb312fb20f49fa12b05c67d
Aaron T <troskaaaron@gmail.com>
2020-07-15 19:34:27 -0500

    working on yup form validation

--- team-builder-app/src/components/form.js
+++ team-builder-app/src/components/form.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import * as Yup from 'yup';
 import { useSelector, useDispatch } from 'react-redux';
-import { Row, Col, Button } from 'reactstrap';
-import { Formik, Form, useFormik } from 'formik';
+import { Row, Col, Button, Container } from 'reactstrap';
+import { Formik, Form, useFormik, Field } from 'formik';
 import { TEST } from '../actions'
 
 const FormManager = () => {
@@ -15,17 +15,74 @@ const FormManager = () => {
 
 	const SignupSchema = Yup.object().shape({
 		firstName: Yup.string()
-		  .min(2, 'Too Short!')
-		  .max(50, 'Too Long!')
-		  .required('Required'),
+			.min(2, 'FName Too Short!')
+			.max(50, 'FName Too Long!')
+			.required('FName Required'),
 		lastName: Yup.string()
-		  .min(2, 'Too Short!')
-		  .max(50, 'Too Long!')
-		  .required('Required'),
-		email: Yup.string()
-		  .email('Invalid email')
-		  .required('Required'),
-	  });
+			.min(2, 'LName Too Short!')
+			.max(50, 'LName Too Long!')
+			.required('LName Required'),
+		});
+		
+	return (
+		<Container>
+			<Formik
+			initialValues={{
+				firstName: '',
+				lastName: '',
+				email: '',
+			}}
+			validationSchema={SignupSchema}
+			onSubmit={ value => {
+				console.log(value)
+				dispatch({
+					type: TEST,
+					payload: value
+				})
+			}}>
+
+			{({ errors, touched }) => (
+				<Form>
+					<div>
+						<Row>
+					 		{errors.firstName ? console.log(errors) : <div>{errors.firstName}</div>}
+					 	</Row>
+
+					<Row>
+						<Col>
+							<label htmlFor="name">First Name </label> 
+						</Col>
+						<Col>
+							<Field 
+								id='firstName'
+								name='firstName'
+								onChange={Formik.handleChange}
+								value={Formik.firstName} />
+						</Col>
+					</Row>
+				</div>
+				<div id="LastName">
+
+					<Row>
+						<Col>
+							<label htmlFor="name">Last Name </label> 
+						</Col>
+						<Col>
+							<input 
+								id='lastName'
+								name='lastName'
+								type='text'
+								onChange={Formik.handleChange}
+								value={Formik.lastName} />
+						</Col>
+					</Row>
+				</div>
+				<Button type='submit' onClick={Formik.handleSubmit}>Submit</Button>
+				</Form>
+			)}</Formik>
+		</Container>
+
+	)
 
 	const validate = (values, props /* only available when using withFormik */) => {
 
@@ -48,66 +105,66 @@ const FormManager = () => {
 		// });
 	};
 
-	const Formik = useFormik({
-		initialValues: {
-			firstName: fName,
-			lastName: '',
-			email: '',
-		},
-		validate,
-		onSubmit: value => {
-			dispatch({
-				type: TEST,
-				payload: value
-			})
-		},
-	})
+	// const Formik = useFormik({
+	// 	initialValues: {
+	// 		firstName: fName,
+	// 		lastName: '',
+	// 		email: '',
+	// 	},
+	// 	validate,
+	// 	onSubmit: value => {
+	// 		dispatch({
+	// 			type: TEST,
+	// 			payload: value
+	// 		})
+	// 	},
+	// })
 
-	return (
-		<form onSubmit={Formik.handleSubmit}>
-			<div>
-				<Row>
-					{Formik.errors.firstName ? <div>{Formik.errors.firstName}</div> : null}
-				</Row>
+	// return (
+	// 	<form onSubmit={Formik.handleSubmit}>
+			// <div>
+			// 	<Row>
+			// 		{Formik.errors.firstName ? <div>{Formik.errors.firstName}</div> : null}
+			// 	</Row>
 
-				<Row>
-					<Col>
-						<label htmlFor="name">First Name </label> 
-					</Col>
-					<Col>
-						<input 
-							id='firstName'
-							name='firstName'
-							type='text'
-							onChange={Formik.handleChange}
-							value={Formik.values.firstName} />
-					</Col>
-				</Row>
-			</div>
-			<div id="LastName">
-				<Row>
-					{Formik.errors.lastName ? <div>{Formik.errors.lastName}</div> : null}
-				</Row>
+			// 	<Row>
+			// 		<Col>
+			// 			<label htmlFor="name">First Name </label> 
+			// 		</Col>
+			// 		<Col>
+			// 			<input 
+			// 				id='firstName'
+			// 				name='firstName'
+			// 				type='text'
+			// 				onChange={Formik.handleChange}
+			// 				value={Formik.values.firstName} />
+			// 		</Col>
+			// 	</Row>
+			// </div>
+			// <div id="LastName">
+			// 	<Row>
+			// 		{Formik.errors.lastName ? <div>{Formik.errors.lastName}</div> : null}
+			// 	</Row>
 
-				<Row>
-					<Col>
-						<label htmlFor="name">Last Name </label> 
-					</Col>
-					<Col>
-						<input 
-							id='lastName'
-							name='lastName'
-							type='text'
-							onChange={Formik.handleChange}
-							value={Formik.values.lastName} />
-					</Col>
-				</Row>
-			</div>
+			// 	<Row>
+			// 		<Col>
+			// 			<label htmlFor="name">Last Name </label> 
+			// 		</Col>
+			// 		<Col>
+			// 			<input 
+			// 				id='lastName'
+			// 				name='lastName'
+			// 				type='text'
+			// 				onChange={Formik.handleChange}
+			// 				value={Formik.values.lastName} />
+			// 		</Col>
+			// 	</Row>
+			// </div>
 
 
-			<Button type='submit' onClick={Formik.handleSubmit}>Submit</Button>
-		</form>
-	)
+			// <Button type='submit' onClick={Formik.handleSubmit}>Submit</Button>
+	// 	</form>
+	// )
 }
 
 export default FormManager
