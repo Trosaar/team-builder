4862505ebb172ca00c147f86c03f0578c0a0c2bc
Aaron T <troskaaaron@gmail.com>
2020-08-10 20:19:39 -0500

    working on conditional rendering

--- team-builder-app/src/components/dash.js
+++ team-builder-app/src/components/dash.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { Container, Row, Col, Button, Form } from 'reactstrap';
 import { useFormik } from 'formik';
 import { TEST } from '../actions';
@@ -7,12 +8,22 @@ import FormManager from  './form.js'
 import List from './list.js'
 
 const Dashboard = () => {
-	return (
-		<Container>
-			<List />
-			<FormManager />
-		</Container>
-	)
+
+	const loggedIn = useSelector(state => state.loggedIn)
+	const history = useHistory()
+
+	if (loggedIn) {
+		return (
+			<Container>
+				<List />
+				<FormManager />
+			</Container>
+		)
+	} else {
+		return(
+			<span>You must log in first</span>
+		)
+	}
 }
 
 export default Dashboard
